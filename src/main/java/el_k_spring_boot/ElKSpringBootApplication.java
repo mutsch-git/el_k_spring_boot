e0cd4bfd08bbfddb72be46a38b14ac6d82d038fa
Atari Oberon <atari-oberon@mutsch-laptop.atari-oberon>
2025-01-07 09:15:09 +0100

    created spring boot project

---
+++ src/main/java/el_k_spring_boot/ElKSpringBootApplication.java
@@ -0,0 +1,13 @@
+package el_k_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElKSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElKSpringBootApplication.class, args);
+	}
+
+}
